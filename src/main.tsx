03b95adcf442d7cd94d39c6f448e8f6d1c3299d0
frenzy2004 <205901597+frenzy2004@users.noreply.github.com>
2026-08-09 16:59:08 +0800

    Fix Vercel static deployment

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import { SmartWatchClone } from "../app/SmartWatchClone";
+import "../app/globals.css";
+
+createRoot(document.getElementById("root")!).render(<SmartWatchClone />);
